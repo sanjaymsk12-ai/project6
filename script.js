157db9dde4dd23dd589b1d0d464e6d9a1223d074
sanjaymsk12-ai <sanjaymsk12@gmail.com>
2025-11-26 21:26:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let arr = [{
+    image:'/sanjay.jpg',
+    name : 'Sanjay',
+    age: '27',
+    maried : 'False',},
+
+{   image:'/Rinku.jpg',
+    name : 'Rinku',
+    age: '22',
+    maried : 'False',},
+
+{   image:'/arshil.jpg',
+    name : 'Arshil',
+    age: '26',
+    maried : 'False',},
+
+{   image:'/ramji.jpg',
+    name : 'Ram ji',
+    age: '28',
+    maried : 'False',}]
+
+sum = ''
+
+arr.forEach(function(elem,idx){
+    sum = sum + `<div class="card">
+        <img src="${elem.image}" alt="image not found">
+        <h1>${elem.name}</h1>
+        <h4>${elem.age}</h4>
+        <h5>${elem.maried}</h5>
+    </div>`
+})
+
+let body = document.querySelector('body')
+body.innerHTML = sum
